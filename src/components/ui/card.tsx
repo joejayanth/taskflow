7129519480258dfc087bfa71efd8065cc343e8e5
Joseph Jayanth <samueljayanth@gmail.com>
2025-11-07 08:50:14 +0000

    can we make the borders a sketchy

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground",
+      "rounded-lg border-2 border-foreground bg-card text-card-foreground",
       className
     )}
     {...props}
