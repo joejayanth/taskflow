706bf60d0c00d1648193993523ebe93a696cdd62
Joseph Jayanth <samueljayanth@gmail.com>
2025-11-27 12:29:10 +0000

    in safari, im not able to edit the due date

--- src/components/task-dialog.tsx
+++ src/components/task-dialog.tsx
@@ -32,6 +32,7 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
+  PopoverPortal,
 } from '@/components/ui/popover';
 import {
   Select,
@@ -333,15 +334,17 @@ export function TaskDialog({ task, trigger, onSave, onDelete, initialStatus, ini
                                       </Button>
                                     </FormControl>
                                   </PopoverTrigger>
-                                  <PopoverContent className="w-auto p-0" align="start">
-                                    <Calendar
-                                      mode="single"
-                                      selected={field.value}
-                                      onSelect={field.onChange}
-                                      disabled={(date) => date < new Date('1900-01-01')}
-                                      initialFocus
-                                    />
-                                  </PopoverContent>
+                                  <PopoverPortal>
+                                    <PopoverContent className="w-auto p-0" align="start">
+                                      <Calendar
+                                        mode="single"
+                                        selected={field.value}
+                                        onSelect={field.onChange}
+                                        disabled={(date) => date < new Date('1900-01-01')}
+                                        initialFocus
+                                      />
+                                    </PopoverContent>
+                                  </PopoverPortal>
                                 </Popover>
                               <FormMessage />
                             </FormItem>
@@ -416,14 +419,16 @@ export function TaskDialog({ task, trigger, onSave, onDelete, initialStatus, ini
                                     </Button>
                                   </FormControl>
                                 </PopoverTrigger>
-                                <PopoverContent className="w-auto p-0" align="start">
-                                  <Calendar
-                                    mode="single"
-                                    selected={field.value}
-                                    onSelect={field.onChange}
-                                    initialFocus
-                                  />
-                                </PopoverContent>
+                                <PopoverPortal>
+                                  <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                      mode="single"
+                                      selected={field.value}
+                                      onSelect={field.onChange}
+                                      initialFocus
+                                    />
+                                  </PopoverContent>
+                                </PopoverPortal>
                               </Popover>
                               {field.value && (
                                 <Button
@@ -522,4 +527,5 @@ export function TaskDialog({ task, trigger, onSave, onDelete, initialStatus, ini
   );
 }
 
+    
     
